1c43bf71259977c86e90449c66a0d0239f267614
Igor Volnukhin <zybu@live.ru>
2020-04-08 16:33:28 +0300

    Java ready

--- src/main.java
+++ src/main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String[] args) {
+
+        int ticket = 15000;  // здесь вводить стоимость билета
+        int bonus = ticket / 20;
+        System.out.printf("Bonus = " + bonus);
+    }
 }
+
